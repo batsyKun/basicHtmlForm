f9f267571f1664efb9afc1683566150804188e55
batsyKun <119741750+batsyKun@users.noreply.github.com>
2023-08-09 16:52:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const form = document.querySelector("form");
   
-    form.addEventListener("submit", function(event) {
-      event.preventDefault(); 
+//     form.addEventListener("submit", function(event) {
+//       event.preventDefault(); 
   
-      const formData = new FormData(form);
+//       const formData = new FormData(form);
   
-      console.log("Form Data:");
-      for (const [name, value] of formData.entries()) {
-        console.log(name + ": " + value);
-      }
+//       console.log("Form Data:");
+//       for (const [name, value] of formData.entries()) {
+//         console.log(name + ": " + value);
+//       }
+//     });
+//   });
+  
+  document.addEventListener("DOMContentLoaded", function() {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const formData = new FormData(form);
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+      formDataObject[key] = value;
     });
+
+    fetch("https://api-generator.retool.com/4bmT4I/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formDataObject)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Response Data:", data);
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
   });
-  
-  
+});
   
   
   
